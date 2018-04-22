5bacdd0dfd64427d8ea06e1224589c9738f1ffff
jenrinaldo <jenrinaldo08@gmail.com>
2018-04-22 23:13:18 +0700

    Bug fix

--- footer.php
+++ footer.php
@@ -78,158 +78,156 @@
 </div>
 
 <script type="text/javascript">
-            var th_name = 'post_title';
-            var ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>';
-            
-            function animelist_ajax(page, th_name){
-                var post_data = {
-                    page: page,
-                    th_name: th_name
-                };
-                
-                $('form.post-list input').val(JSON.stringify(post_data));
-                var cek = $('form.post-list input').val();
-                if(typeof cek!=='undefined'){
-                    var data = {
-                    action: "animelist_ajax",
-                    data: JSON.parse(cek)
-                };
-                }
-                
-                
-                $.post(ajaxurl, data, function(response) {
-                        // If successful Append the data into our html container
-                        $(".animelist_container").html(response);
-                        // End the transition
-                        $(".animelist_page_loading").css({'background':'none', 'transition':'all 1s ease-out'});
-                    });
-            } 
-            
-            jQuery(document).ready(function($) {                                                                
-                
-                // Initialize default item to sort and it's sort order
-                
-                // Check if our hidden form input is not empty, meaning it's not the first time viewing the page.
-                if($('form.post-list input').val()){
-                    // Submit hidden form input value to load previous page number
-                    data = JSON.parse($('form.post-list input').val());
-                    animelist_ajax(data.page, data.th_name);
-                } else {
-                    // Load first page
-                    animelist_ajax(1, 'post_title');
-                }
-                
-                var th_active = $('.table-post-list th.active');
-                var th_name = $(th_active).attr('id');
-                
-                // Pagination Clicks                    
-                $(document).on('click','.animelist_container .animelist_pagination li.active',function(){
-                    var page = $(this).attr('p');
-                    animelist_ajax(page, th_name); 
-                }); 
-                $('.navlist a').click(function() {
-                var text = $(this).attr('data-id');
-                if(text=='all'){
-                    animelist_ajax(1, 'post_title');
-                } else {
-                    animelist_ajax(1, text);
-                }
-                });
-            }); 
-            </script>
-<script>
-        $('.ongoing_holder').slick({
-        lazyLoad: 'ondemand',
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 680,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 380,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
+$('.ongoing_holder').slick({
+    lazyLoad: 'ondemand',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
             }
-        ]
-    });
-</script>
-
-<script async type='text/javascript'>
-$(function() {
-var showTotalChar = 200, hideChar = "Show More (+)", showChar = "Show Less (-)";
-$('.show').each(function() {
-var content = $(this).text();
-if (content.length > showTotalChar) {
-var con = content.substr(0, showTotalChar);
-var hcon = content.substr(showTotalChar, content.length - showTotalChar);
-var txt= con +  '<span class="dots"></span><span class="morectnt"><span>' + hcon + '</span>&nbsp;&nbsp;<div class="showmoretxt"><a href="">' + showChar + '</a></div></span>';
-$(this).html(txt);
-}
+        },
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 380,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
-$(".showmoretxt").click(function() {
-if ($(this).hasClass("sample")) {
-$(this).removeClass("sample");
-$(this).text(showChar);
-} else {
-$(this).addClass("sample");
-$(this).text(hideChar);
-}
-$(this).parent().prev().toggle();
-$(this).prev().toggle();
-return false;
-});
-});
-</script>
-<script async type="text/javascript">
 var URLs = 'index';
- jQuery(document).ready(function(n) {
-         var a = "<?php echo admin_url('admin-ajax.php'); ?>";
-     function c(c) {
-         n(".index_loading").fadeIn().css("background", "#ccc");
-         var i = {
-             page: c,
-             action: "index_ajax",
-             'post_id': URLs,
-         };
-         n.post(a, i, function(a) {
-             n(".index_container").html(a), n(".index_loading").css({
-                 background: "none",
-                 transition: "all 1s ease-out"
-             })
-         })
-     }
-     c(1), n(document).on("click",".index_container .index_paginations li.active", function() {
-         c(n(this).attr("p"))
-     })
- });</script>
- <script async>
+jQuery(document).ready(function(n) {
+    var a = "<?php echo admin_url('admin-ajax.php'); ?>";
+
+    function c(c) {
+        n(".index_loading").fadeIn().css("background", "#ccc");
+        var i = {
+            page: c,
+            action: "index_ajax",
+            'post_id': URLs,
+        };
+        n.post(a, i, function(a) {
+            n(".index_container").html(a), n(".index_loading").css({
+                background: "none",
+                transition: "all 1s ease-out"
+            })
+        })
+    }
+    c(1), n(document).on("click", ".index_container .index_paginations li.active", function() {
+        c(n(this).attr("p"))
+    })
+});
+var th_name = 'post_title';
+var ajaxurl = '<?php echo admin_url('
+admin - ajax.php '); ?>';
+
+function animelist_ajax(page, th_name) {
+    var post_data = {
+        page: page,
+        th_name: th_name
+    };
+
+    $('form.post-list input').val(JSON.stringify(post_data));
+    var cek = $('form.post-list input').val();
+    if (typeof cek !== 'undefined') {
+        var data = {
+            action: "animelist_ajax",
+            data: JSON.parse(cek)
+        };
+    }
+    $.post(ajaxurl, data, function(response) {
+        // If successful Append the data into our html container
+        $(".animelist_container").html(response);
+        // End the transition
+        $(".animelist_page_loading").css({
+            'background': 'none',
+            'transition': 'all 1s ease-out'
+        });
+    });
+}
+
+jQuery(document).ready(function($) {
+
+    // Initialize default item to sort and it's sort order
+
+    // Check if our hidden form input is not empty, meaning it's not the first time viewing the page.
+    if ($('form.post-list input').val()) {
+        // Submit hidden form input value to load previous page number
+        data = JSON.parse($('form.post-list input').val());
+        animelist_ajax(data.page, data.th_name);
+    } else {
+        // Load first page
+        animelist_ajax(1, 'post_title');
+    }
+
+    var th_active = $('.table-post-list th.active');
+    var th_name = $(th_active).attr('id');
+
+    // Pagination Clicks                    
+    $(document).on('click', '.animelist_container .animelist_pagination li.active', function() {
+        var page = $(this).attr('p');
+        animelist_ajax(page, th_name);
+    });
+    $('.navlist a').click(function() {
+        var text = $(this).attr('data-id');
+        if (text == 'all') {
+            animelist_ajax(1, 'post_title');
+        } else {
+            animelist_ajax(1, text);
+        }
+    });
+});
+$(function() {
+    var showTotalChar = 200,
+        hideChar = "Show More (+)",
+        showChar = "Show Less (-)";
+    $('.show').each(function() {
+        var content = $(this).text();
+        if (content.length > showTotalChar) {
+            var con = content.substr(0, showTotalChar);
+            var hcon = content.substr(showTotalChar, content.length - showTotalChar);
+            var txt = con + '<span class="dots"></span><span class="morectnt"><span>' + hcon + '</span>&nbsp;&nbsp;<div class="showmoretxt"><a href="">' + showChar + '</a></div></span>';
+            $(this).html(txt);
+        }
+    });
+    $(".showmoretxt").click(function() {
+        if ($(this).hasClass("sample")) {
+            $(this).removeClass("sample");
+            $(this).text(showChar);
+        } else {
+            $(this).addClass("sample");
+            $(this).text(hideChar);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+
 var $ = jQuery.noConflict();
 $(function() {
     $("#activator").click(function() {
@@ -245,107 +243,103 @@ $(function() {
     $(".toggle_container").hide(), $(".trigger").click(function() {
         return $(this).toggleClass("active").next().slideToggle("slow"), !1
     })
-}), $(document).ready(function() {
-    var o = $(".wp-pagenavi");
-    console.log(o), 0 != o.length && ($("span.current").remove(), $("a.page.larger").remove(), $("a.page.smaller").remove(), $("a.previouspostslink").css({
-        "font-size": "1.5rem"
-    }), $("a.nextpostslink").css({
-        "font-size": "1.5rem"
-    }))
 });
-</script>
-<script>
 $(document).ready(function() {
-    $('#list').click(function(event){event.preventDefault();
-    $('#products .item').removeClass('gridmode');
-    $('#products .item').addClass('list');});
-    $('#grid').click(function(event){event.preventDefault();
-    $('#products .item').removeClass('list');
-    $('#products .item').addClass('gridmode');});
+    $('#list').click(function(event) {
+        event.preventDefault();
+        $('#products .item').removeClass('gridmode');
+        $('#products .item').addClass('list');
+    });
+    $('#grid').click(function(event) {
+        event.preventDefault();
+        $('#products .item').removeClass('list');
+        $('#products .item').addClass('gridmode');
+    });
 });
 $(document).ready(function() {
     $(".seasonss").hide();
     $(".genres").hide();
-$('.navlist2 a').click(function(event){event.preventDefault();
-                var text = $(this).attr('data-id');
-                if(text=='Season'){
-                    $(".seasonss").show();
-                    $(".genres").hide();
-                } else if(text=="Genre"){
-                    $(".seasonss").hide();
-                    $(".genres").show();
-                }
-});
+    $('.navlist2 a').click(function(event) {
+        event.preventDefault();
+        var text = $(this).attr('data-id');
+        if (text == 'Season') {
+            $(".seasonss").show();
+            $(".genres").hide();
+        } else if (text == "Genre") {
+            $(".seasonss").hide();
+            $(".genres").show();
+        }
+    });
 
-$('.warnalist li').click(function(event){event.preventDefault();
-                var text = $(this).attr('class');
-                if(text=='initv'){
-                    $(".soralist li").removeClass('active');
-                    $( "li.tv" ).addClass('active');
-                }  else if(text=='iniova'){
-                    $(".soralist li").removeClass('active');
-                    $( "li.ova" ).addClass('active');
-                }  else if(text=='inimovie'){
-                    $(".soralist li").removeClass('active');
-                    $( "li.movie" ).addClass('active');
-                }  else if(text=='inispecial'){
-                    $(".soralist li").removeClass('active');
-                    $( "li.special" ).addClass('active');
-                }   else if(text=='initvova'){
-                    $(".soralist li").removeClass('active');
-                    $( "li.tvova" ).addClass('active');
-                }  else if(text=='initvspecial'){
-                    $(".soralist li").removeClass('active');
-                    $( "li.tvspesial" ).addClass('active');
-                }
-});
+    $('.warnalist li').click(function(event) {
+        event.preventDefault();
+        var text = $(this).attr('class');
+        if (text == 'initv') {
+            $(".soralist li").removeClass('active');
+            $("li.tv").addClass('active');
+        } else if (text == 'iniova') {
+            $(".soralist li").removeClass('active');
+            $("li.ova").addClass('active');
+        } else if (text == 'inimovie') {
+            $(".soralist li").removeClass('active');
+            $("li.movie").addClass('active');
+        } else if (text == 'inispecial') {
+            $(".soralist li").removeClass('active');
+            $("li.special").addClass('active');
+        } else if (text == 'initvova') {
+            $(".soralist li").removeClass('active');
+            $("li.tvova").addClass('active');
+        } else if (text == 'initvspecial') {
+            $(".soralist li").removeClass('active');
+            $("li.tvspesial").addClass('active');
+        }
+    });
 
 });
-$(document).ready( function() {
+$(document).ready(function() {
     /* Check width on page load*/
-    if ( $(window).width() < 480) {
-     $('#products .item').addClass('gridmode');
-     $('#products .item').removeClass('list');
+    if ($(window).width() < 480) {
+        $('#products .item').addClass('gridmode');
+        $('#products .item').removeClass('list');
     }
-    if ( $(window).width() < 555) {
-     $('a.btn.facebook').text('');
-     $('a.btn.twitter').text('');
-     $('a.btn.google').text('');
-     $('a.btn.facebook').append('<i class="fa fa-facebook"></i>');
-     $('a.btn.twitter').append('<i class="fa fa-twitter"></i>');
-     $('a.btn.google').append('<i class="fa fa-google-plus"></i>');
+    if ($(window).width() < 555) {
+        $('a.btn.facebook').text('');
+        $('a.btn.twitter').text('');
+        $('a.btn.google').text('');
+        $('a.btn.facebook').append('<i class="fa fa-facebook"></i>');
+        $('a.btn.twitter').append('<i class="fa fa-twitter"></i>');
+        $('a.btn.google').append('<i class="fa fa-google-plus"></i>');
     }
- });
+});
 
- $(window).resize(function() {
+$(window).resize(function() {
     /*If browser resized, check width again */
     if ($(window).width() < 470) {
-     $('#products .item').addClass('gridmode');
-     $('#products .item').removeClass('list');
+        $('#products .item').addClass('gridmode');
+        $('#products .item').removeClass('list');
+    } else {
+        $('#products .item').addClass('list');
+        $('#products .item').removeClass('gridmode');
     }
-    else {
-     $('#products .item').addClass('list');
-     $('#products .item').removeClass('gridmode');
+    if ($(window).width() < 555) {
+        $('a.btn.facebook').text('');
+        $('a.btn.twitter').text('');
+        $('a.btn.google').text('');
+        $('a.btn.facebook').append('<i class="fa fa-facebook"></i>');
+        $('a.btn.twitter').append('<i class="fa fa-twitter"></i>');
+        $('a.btn.google').append('<i class="fa fa-google-plus"></i>');
+    } else {
+        $('a.btn.facebook').text('Facebook');
+        $('a.btn.twitter').text('Twitter');
+        $('a.btn.google').text('Google+');
+        $('a.btn.facebook i').remove();
+        $('a.btn.twitter i').remove();
+        $('a.btn.google i').remove();
     }
-    if ( $(window).width() < 555) {
-     $('a.btn.facebook').text('');
-     $('a.btn.twitter').text('');
-     $('a.btn.google').text('');
-     $('a.btn.facebook').append('<i class="fa fa-facebook"></i>');
-     $('a.btn.twitter').append('<i class="fa fa-twitter"></i>');
-     $('a.btn.google').append('<i class="fa fa-google-plus"></i>');
-    }else {
-     $('a.btn.facebook').text('Facebook');
-     $('a.btn.twitter').text('Twitter');
-     $('a.btn.google').text('Google+');
-     $('a.btn.facebook i').remove();
-     $('a.btn.twitter i').remove();
-     $('a.btn.google i').remove();
-    }
- });
- if ($('.top').length) {
+});
+if ($('.top').length) {
     var scrollTrigger = 100, // px
-        backToTop = function () {
+        backToTop = function() {
             var scrollTop = $(window).scrollTop();
             if (scrollTop > scrollTrigger) {
                 $('.top').addClass('show');
@@ -354,17 +348,16 @@ $(document).ready( function() {
             }
         };
     backToTop();
-    $(window).on('scroll', function () {
+    $(window).on('scroll', function() {
         backToTop();
     });
-    $('.top').on('click', function (e) {
+    $('.top').on('click', function(e) {
         e.preventDefault();
         $('html,body').animate({
             scrollTop: 0
         }, 700);
     });
 }
-
 </script>
 </BODY>
 </html>
