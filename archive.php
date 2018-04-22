55f8f5a9777e1408d97db28c89e953a029e6a20c
jenrinaldo <jenrinaldo08@gmail.com>
2018-04-22 20:32:41 +0700

    Update

--- archive.php
+++ archive.php
@@ -1,29 +1,37 @@
-<?php get_header(); ?>
+<?php get_header();?>
 
 <div class="venser">
 
-<div class="rvad"><h1><span><?php single_tag_title(); ?></span></h1></div>
-<div class="rapi">
-<div class="venz">
-<ul>
-<?php if (have_posts()) : ?>
-                    <?php while (have_posts()) : the_post(); ?>
-                        <?php include (TEMPLATEPATH . '/grid.php'); ?>
-                    <?php endwhile; ?>
-            <?php else : ?>
-                    <h3><?php _e('No Post Found', 'cmeasytheme'); ?></h3>
-            <?php endif; ?>
-</ul>
-
+<div class="rvad">
+        <h1>
+                <span>
+                        <?php single_tag_title();?>
+                </span>
+        </h1>
 </div>
+<div class="rapi">
+        <div class="venz">
+                <ul>
+                        <?php
+                        if (have_posts()):
+                                while (have_posts()): the_post();
+                                include TEMPLATEPATH . '/grid.php';
+                        endwhile;
+                        else:
+                                echo '<h3>';
+                                _e('No Post Found');
+                                echo '</h3>';
+                        endif;
+                        ?>
+                </ul>
+        </div>
 </div>
 <div class="clear"></div>
 <div class="pagination">
-<?php pagenavi(); ?>
+        <?php pagenavi();?>
+</div>
+</div>
+<?php get_sidebar()?>
 </div>
 
-</div>
-<?php get_sidebar() ?>
-</div>
-
-<?php get_footer(); ?>
+<?php get_footer();?>
