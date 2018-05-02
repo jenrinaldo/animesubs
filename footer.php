39b8d85dee04b55eaa023e54ad0695680e6a6236
jenrinaldo <jenrinaldo08@gmail.com>
2018-05-03 06:20:15 +0700

    update

--- footer.php
+++ footer.php
@@ -386,8 +386,45 @@ new Vue({
     message: 'Akanime.net'
   }
 })
+</script>
+<script>
+    var apiURL = '/wp-json/wp/v2/anime-api/?_embed&per_page=10'
 
+/**
+ * Posts demo with ability to change author
+ */
 
+var posts = new Vue({
+
+	el: '#app',
+
+	data: {
+		authors: ['1', '590'],
+		currentAuthor: '1',
+		posts: null
+	},
+
+	created: function() {
+		this.fetchData()
+	},
+
+	watch: {
+		currentAuthor: 'fetchData'
+	},
+
+	methods: {
+		fetchData: function() {
+			var xhr = new XMLHttpRequest()
+			var self = this
+			xhr.open('GET', apiURL + self.currentAuthor)
+			xhr.onload = function() {
+				self.posts = JSON.parse(xhr.responseText)
+				console.log(self.posts[0].link)
+			}
+			xhr.send()
+		}
+	}
+})
 </script>
 </BODY>
 </html>
