7cee6025b2e82a0ec69ffd3f2dc27d3f701e5efa
jenrinaldo <jenrinaldo08@gmail.com>
2018-05-02 17:29:59 +0700

    update

--- footer.php
+++ footer.php
@@ -202,32 +202,7 @@ $('.ongoing_holder').slick({
         }
     ]
 });
-$(function() {
-    var showTotalChar = 200,
-        hideChar = "Show More (+)",
-        showChar = "Show Less (-)";
-    $('.show').each(function() {
-        var content = $(this).text();
-        if (content.length > showTotalChar) {
-            var con = content.substr(0, showTotalChar);
-            var hcon = content.substr(showTotalChar, content.length - showTotalChar);
-            var txt = con + '<span class="dots"></span><span class="morectnt"><span>' + hcon + '</span>&nbsp;&nbsp;<div class="showmoretxt"><a href="">' + showChar + '</a></div></span>';
-            $(this).html(txt);
-        }
-    });
-    $(".showmoretxt").click(function() {
-        if ($(this).hasClass("sample")) {
-            $(this).removeClass("sample");
-            $(this).text(showChar);
-        } else {
-            $(this).addClass("sample");
-            $(this).text(hideChar);
-        }
-        $(this).parent().prev().toggle();
-        $(this).prev().toggle();
-        return false;
-    });
-});
+
 
 var $ = jQuery.noConflict();
 $(function() {
